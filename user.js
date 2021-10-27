bf4a4893978786bd438237cdbca086a6581feaa3
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-10-27 13:22:33 +0530

    Update user.js

--- user.js
+++ user.js
@@ -8,10 +8,45 @@ user_pref("devtools.onboarding.telemetry.logged", false);
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.server", "");
 user_pref("toolkit.telemetry.unified", false);
+user_pref("datareporting.healthreport.uploadEnabled", false)
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("browser.ping-centre.telemetry", false);
 
-// Disable Pocket
+// Disable studies and Normandy/Shield
+user_pref("datareporting.policy.dataSubmissionEnable", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+// Crash reports are left as is / not disabled
+
+// Disable activity on new tab pages
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.newtab.preload", true);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);
 user_pref("extensions.pocket.enabled", false);
+
+// Disable safe browsing
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "");
+
+// Set Language
+user_pref("intl.accept_languages", "en-US, en")
+
+// Disable auto update
+user_pref("app.update.background.scheduling.enabled", false);
 
 // Disable Prefetching [L1]
 user_pref("network.dns.disablePrefetch", true);
