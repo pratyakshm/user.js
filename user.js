730b47af2ea6d15f20b38e576ca0c6f187b30e1a
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2022-06-28 03:58:41 +0000

    Set browser.startup.page to 1 [Home]

--- user.js
+++ user.js
@@ -84,7 +84,7 @@
   * 0=blank, 1=home, 2=last visited page, 3=resume previous session
   * [NOTE] Session Restore is cleared with history (2811, 2812), and not used in Private Browsing mode
   * [SETTING] General>Startup>Restore previous session ***/
- user_pref("browser.startup.page", 0);
+ user_pref("browser.startup.page", 1);
  /* 0103: set HOME+NEWWINDOW page
   * about:home=Activity Stream (default, see 0105), custom URL, about:blank
   * [SETTING] Home>New Windows and Tabs>Homepage and new windows ***/
